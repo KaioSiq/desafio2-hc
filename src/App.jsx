9b02d5a6aef8aa1b1fe92d4bca8ee5993d4c998a
Kaio <kaio.siqueira@poli,ufrj.br>
2021-07-25 19:18:00 -0300

    Fix lint errors

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Texto</Button>
+    </div>
+  );
+}
+
+export default App;
